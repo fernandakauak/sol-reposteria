05fa254d319fba2073c2ef21ac116bf0b7c6d8d2
Fernanda Kauak <fernanda.kauak@gmail.com>
2019-07-10 23:43:41 -0400

    Inicio secciones contacto y productos

--- contacto.php
+++ contacto.php
@@ -1,7 +1,16 @@
 <?php include("header.php"); ?>
 
-<content class="contact">
+<div class="contact">
   <h1>Contacto</h1>
-</content>
+  <img class="img col-lg-3 offset-lg-1" src="./img/sol.png" />
+  <div class="info col-lg-4">
+  	<p>Si quieres revisar más de nuestros productos y ponerte en contacto con nosotros, puedes ver nuestras redes:</p>
+  	<ul>
+  		<li>+569 5451 4286</li>
+  		<li>@solreposteriacasera</li>
+  		<li>solreposteriacasera@gmail.com</li>
+  	</ul>
+  </div>
+</div>
 
 <?php include('footer.php') ?>
--- productos.php
+++ productos.php
@@ -1,7 +1,31 @@
 <?php include("header.php"); ?>
 
-<content class="portfolio">
+<div class="portfolio">
   <h1>Productos</h1>
-</content>
+  <div class="col-lg-12">
+  	<h2>Pasteles</h2>
+  	<div class="col-lg-12" style="margin-bottom:3%">
+  		<div class="col-lg-4" style="height:250px;background-color:#000;float:left;"></div>
+		<div class="col-lg-4" style="height:250px;background-color:#000;float:left;"></div>
+		<div class="col-lg-4" style="height:250px;background-color:#000;float:left;"></div>
+  	</div>
+  </div>
+	<div class="col-lg-12">
+  	<h2>Dulces</h2>
+  	<div class="col-lg-12" style="margin-bottom:3%">
+  		<div class="col-lg-4" style="height:250px;background-color:#000;float:left;"></div>
+		<div class="col-lg-4" style="height:250px;background-color:#000;float:left;"></div>
+		<div class="col-lg-4" style="height:250px;background-color:#000;float:left;"></div>
+  	</div>
+  </div>
+  <div class="col-lg-12">
+  	<h2>Pan de Pascua</h2>
+  	<div class="col-lg-12" style="margin-bottom:3%">
+  		<div class="col-lg-4" style="height:250px;background-color:#000;float:left;"></div>
+		<div class="col-lg-4" style="height:250px;background-color:#000;float:left;"></div>
+		<div class="col-lg-4" style="height:250px;background-color:#000;float:left;"></div>
+  	</div>
+  </div>
+</div>
 
 <?php include('footer.php') ?>
